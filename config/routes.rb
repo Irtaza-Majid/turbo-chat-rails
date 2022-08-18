ee10670d58ee23721acc863f2a99b1fba4e60c3a
Irtaza <irtaza.majid@devsinc.com>
2022-08-18 16:10:15 +0500

    adding devise avatar to the rails turbo chat app

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  get 'users/show'
+
+  root 'pages#home'
   resources :rooms do
     resources :messages
   end
-  root 'pages#home'
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  devise_scope :users do
-    get 'users', to: 'devise/session#new'
-  end
+  # devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+  }
   get 'user/:id', to: 'users#show', as: 'user'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
 end
